58ec475e455300c9bdc55c5defb520c69640cb3c
Leonardo <leonardo.felipe@outlook.com>
2022-08-18 17:16:50 -0300

    Change view name

--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,6 @@ from rest_framework.generics import ListCreateAPIView
 from tasks.models import Task
 from tasks.serializers import TaskSerializer
 
-class TaskAPIView(ListCreateAPIView):
+class TaskListCreateAPIView(ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
